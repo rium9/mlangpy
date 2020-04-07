4daa9b02b056cfa92dba66fcbe60781bc5b47392
Jamie <jam10@hw.ac.uk>
2020-04-07 14:19:34 +0100

    Added boilerplate so that mlangpy can be upload to PyPi. Changed the way that grammar files are loaded into metaparsers.py (using Lark.open now - should have been using this from the start). MANIFEST.in is required so that lark_grammars are included in the package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlangpy",
+    version="0.0.5",
+    author="Jamie Muir",
+    author_email="jam10@hw.ac.uk",
+    description="A package for parsing and manipulating some standard metalanguages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rium9/mlangpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
